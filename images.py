5add30c0b870b73bb844bcb77c382b63fe064523
GhermanCristian <gherman_cristi10@yahoo.com>
2019-09-18 12:28:36 +0300

    added more sound cues and created levels

--- images.py
+++ images.py
@@ -1,9 +1,11 @@
 import pygame
 import os
+from constants import BADEA_SOUND_PATH, DUMITRESCU_SOUND_PATH, MARLANU_SOUND_PATH,\
+    SILVIU_SOUND_PATH, PROSTAMOL_SOUND_PATH
 
 path = os.path.join(os.getcwd(), "Images")
 ALL_IMAGES = {}
-BADEA = []
+SPECIAL_IMAGES = {}
 
 for file in os.listdir(path):
     imageName = file[:-5].upper() + "_" + file[-5]
@@ -13,6 +15,14 @@ for file in os.listdir(path):
     ALL_IMAGES[imageName] = pygame.image.load(os.path.join(path, file))
     
     if "BADEA" in imageName:
-        BADEA.append(ALL_IMAGES[imageName])
+        SPECIAL_IMAGES[ ALL_IMAGES[imageName] ] = BADEA_SOUND_PATH
+    if "DUMITRESCU" in imageName:
+        SPECIAL_IMAGES[ ALL_IMAGES[imageName] ] = DUMITRESCU_SOUND_PATH
+    if "MARLANU" in imageName:
+        SPECIAL_IMAGES[ ALL_IMAGES[imageName] ] = MARLANU_SOUND_PATH
+    if "SILVIU" in imageName:
+        SPECIAL_IMAGES[ ALL_IMAGES[imageName] ] = SILVIU_SOUND_PATH
+    if "PROSTAMOL" in imageName:
+        SPECIAL_IMAGES[ ALL_IMAGES[imageName] ] = PROSTAMOL_SOUND_PATH
 
-SERGHEI_ICON1 = pygame.image.load(os.path.join(path, "SERGHEI_ICON1.jpg"))
+SERGHEI_ICON1 = pygame.image.load(os.path.join(path, "SERGHEI_ICON1.ICO"))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from constants import *
-from images import ALL_IMAGES, BADEA, SERGHEI_ICON1
+from images import ALL_IMAGES, SPECIAL_IMAGES, SERGHEI_ICON1 
 from music import *
 from pygame.locals import MOUSEBUTTONUP, MOUSEMOTION, QUIT, K_ESCAPE, KEYUP
 
@@ -9,15 +9,18 @@ def generateRevealedBoxesData(val):
         revealedBoxes.append([val] * BOARD_HEIGHT)
     return revealedBoxes
 
-def getRandomizedBoard():
-    # Get a list of every possible image.
+def getRandomizedImageList():
     icons = []
+    
     for image in ALL_IMAGES.values():
         icons.append(image)
+    random.shuffle(icons)
+    
+    return icons
 
-    random.shuffle(icons) # randomize the order of the icons list
+def getRandomizedBoard(imageList, level):
     numImagesUsed = int(BOARD_WIDTH * BOARD_HEIGHT / 2) # calculate how many icons are needed
-    icons = icons[:numImagesUsed] * 2 # make two of each
+    icons = imageList[(level - 1) * numImagesUsed: level * numImagesUsed] * 2 # make two of each
     random.shuffle(icons)
 
     # Create the board data structure, with randomly placed icons.
@@ -107,7 +110,40 @@ def drawHighlightBox(boxx, boxy):
     left, top = leftTopCoordsOfBox(boxx, boxy)
     pygame.draw.rect(DISPLAY, HIGHLIGHT_COLOR, (left - 5, top - 5, BOX_SIZE + 10, BOX_SIZE + 10), 4)
 
-def startGameAnimation(board):
+def sanityChecks():
+    assert (BOARD_WIDTH * BOARD_HEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
+    assert len(ALL_IMAGES) >= BOARD_HEIGHT * BOARD_WIDTH / 2, 'Not enough pictures'
+    
+    #if this happens, I need to do sth like this:
+    #image = pygame.transform.scale(image, (BOX_SIZE, BOX_SIZE))
+    #but I want to be notified if/when this happens, so I'll use assert for now
+    for img in ALL_IMAGES.values():
+        assert(img.get_size() == (BOX_SIZE, BOX_SIZE)), 'The image size is incorrect'
+
+def gameStart():
+    sanityChecks()
+    
+    pygame.init()
+    pygame.display.set_caption(GAME_TITLE)
+    pygame.display.set_icon(SERGHEI_ICON1)
+    
+    global FPS_CLOCK, DISPLAY, MY_FONT
+    FPS_CLOCK = pygame.time.Clock()
+    DISPLAY = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    MY_FONT = pygame.font.SysFont(TEXT_FONT, TEXT_FONT_SIZE, True, False)
+    
+    DISPLAY.fill(BG_COLOR)
+    playMusic()
+    
+    return 1, getRandomizedImageList(), 0, 0
+
+def levelStart(level, imageList):
+    mainBoard = getRandomizedBoard(imageList, level)
+    startLevelAnimation(mainBoard)
+    #nrMoves, nrRevealed, timePassed, mainBoard, revealedBoxes, firstSelection
+    return 0, 0, 0, mainBoard, generateRevealedBoxesData(False), None
+
+def startLevelAnimation(board):
     # Randomly reveal the boxes 10 at a time.
     coveredBoxes = generateRevealedBoxesData(False)
     boxes = []
@@ -122,7 +158,7 @@ def startGameAnimation(board):
         revealBoxesAnimation(board, boxGroup)
         coverBoxesAnimation(board, boxGroup)
         
-def gameWonAnimation(board):
+def levelWonAnimation(board):
     # flash the background color when the player has won
     coveredBoxes = generateRevealedBoxesData(True)
     color1 = LIGHT_BG_COLOR
@@ -135,34 +171,19 @@ def gameWonAnimation(board):
         pygame.display.update()
         pygame.time.wait(300)
 
-def gameWon(board):
-    playSound(SERGHEI_SOUND_PATH, 1.0)
+def levelWon(board, level):
+    if level <= 2:
+        playSound(SERGHEI_SOUND_PATH, 1.0)
+    else:
+        playSound(INTELIGENT_SOUND_PATH, 3.0)
                             
-    gameWonAnimation(board)
+    levelWonAnimation(board)
     pygame.time.wait(2000)
-    
-    # Reset the board
-    board = getRandomizedBoard()
-                            
-    # Replay the start game animation.
-    startGameAnimation(board)
-    
-    return board, generateRevealedBoxesData(False), 0, 0, 0
 
 def quitGame():
     fadeOut()
     pygame.quit()
     quit()
-
-def sanityChecks():
-    assert (BOARD_WIDTH * BOARD_HEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
-    assert len(ALL_IMAGES) >= BOARD_HEIGHT * BOARD_WIDTH / 2, 'Not enough pictures'
-    
-    #if this happens, I need to do sth like this:
-    #image = pygame.transform.scale(image, (BOX_SIZE, BOX_SIZE))
-    #but I want to be notified if/when this happens, so I'll use assert for now
-    for img in ALL_IMAGES.values():
-        assert(img.get_size() == (BOX_SIZE, BOX_SIZE)), 'The image size is incorrect'
 
 def convertTime(ms):
     minutes = ms / 60000
@@ -175,31 +196,10 @@ def displayText(text, xPos, yPos):
     DISPLAY.blit(MY_FONT.render(text, True, TEXT_COLOR, None), (xPos, yPos))
 
 def main():
-    sanityChecks()
+    LEVEL_COUNT = int(2 * len(ALL_IMAGES) / (BOARD_HEIGHT * BOARD_WIDTH))
     
-    global FPS_CLOCK, DISPLAY, MY_FONT
-    
-    pygame.init()
-    pygame.display.set_caption(GAME_TITLE)
-    pygame.display.set_icon(SERGHEI_ICON1)
-    
-    FPS_CLOCK = pygame.time.Clock()
-    DISPLAY = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    MY_FONT = pygame.font.SysFont(TEXT_FONT, TEXT_FONT_SIZE, True, False)
-    
-    mouseX = 0
-    mouseY = 0
-    nrMoves = 0
-    nrRevealed = 0
-    timePassed = 0
-    
-    mainBoard = getRandomizedBoard()
-    revealedBoxes = generateRevealedBoxesData(False)
-    firstSelection = None
-    
-    DISPLAY.fill(BG_COLOR)
-    playMusic()
-    startGameAnimation(mainBoard)
+    level, imageList, mouseX, mouseY = gameStart()
+    nrMoves, nrRevealed, timePassed, mainBoard, revealedBoxes, firstSelection = levelStart(level, imageList)
     
     while True:
         mouseClicked = False
@@ -207,6 +207,7 @@ def main():
         DISPLAY.fill(BG_COLOR)
         drawBoard(mainBoard, revealedBoxes)
         
+        displayText("Level: " + str(level), TEXT_LEFT_MARGIN, 5 * TEXT_TOP_MARGIN)
         displayText("Moves: " + str(nrMoves), TEXT_LEFT_MARGIN, TEXT_TOP_MARGIN)
         displayText(convertTime(timePassed), TEXT_LEFT_MARGIN, WINDOW_HEIGHT - TEXT_TOP_MARGIN - TEXT_FONT_SIZE)
         
@@ -251,10 +252,16 @@ def main():
                         nrRevealed += 2
                         
                         if nrRevealed == BOARD_HEIGHT * BOARD_WIDTH:
-                            mainBoard, revealedBoxes, timePassed, nrRevealed, nrMoves = gameWon(mainBoard)
-                            
-                        elif image1 in BADEA:
-                            playSound(BADEA_SOUND_PATH, 3.0)
+                            levelWon(mainBoard, level)
+                            level += 1
+                            if level <= LEVEL_COUNT:
+                                nrMoves, nrRevealed, timePassed, mainBoard, revealedBoxes, firstSelection = \
+                                levelStart(level, imageList)
+                            else:
+                                quitGame()
+                                
+                        elif image1 in SPECIAL_IMAGES.keys():
+                            playSound(SPECIAL_IMAGES[image1], 3.0)
                                     
                     # reset firstSelection variable            
                     firstSelection = None 
